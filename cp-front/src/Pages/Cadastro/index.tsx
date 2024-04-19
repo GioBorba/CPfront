380e9df41f5146df80720e2dd422c21f7ef7dd7d
Guilherme <guizin05.oliveira@gmail.com>
2024-04-19 09:00:05 -0300

    update code

--- cp-front/src/Pages/Cadastro/index.tsx
+++ cp-front/src/Pages/Cadastro/index.tsx
@@ -1,16 +1,35 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 import './style.css';
 
 const Cadastro = () => {
-    
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleCadastro = async () => {
+        try {
+            const response = await axios.post('https://caiohalbert.bsite.net/api/user/newUser', {
+                username,
+                email,
+                password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao cadastrar:', error);
+        }
+    };
+
     return (
         <div className="corpo-cadastro">
             <div className="form-container">
                 <h1 className="cad">Cadastro</h1>
-                <input type="text" placeholder="Usuário" />
-                <input type="text" placeholder="Email"/>
-                <input type="password" placeholder="Senha" />
-                <button className="button cadastro">
+                <input type="text" placeholder="Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="button cadastro" onClick={handleCadastro}>
                     Cadastrar
                 </button>
             </div>
--- cp-front/src/Pages/Login/index.tsx
+++ cp-front/src/Pages/Login/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './style.css';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('https://caiohalbert.bsite.net/api/user/login', {
+                username,
+                password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao fazer login:', error);
+        }
+    };
+
     return (
         <div className="corpo-login">
             <div className="form-container">
                 <h1 className='log'>Login</h1>
-                <input type="text" placeholder="Usuário" />
-                <input type="password" placeholder="Senha" />
-                <button className="button login">
+                <input type="text" placeholder="Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="button login" onClick={handleLogin}>
                     Logar
                 </button>
             </div>
