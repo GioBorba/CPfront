f7c7ab5adc085e5b35d276430e006a4015337c40
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-19 09:10:50 -0300

    Add login logic Backend

--- cp-front/src/Pages/Cadastro/index.tsx
+++ cp-front/src/Pages/Cadastro/index.tsx
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+ 
 import './style.css';
-
+ 
 const Cadastro = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+ 
     const handleCadastro = async () => {
         try {
             const response = await axios.post('https://caiohalbert.bsite.net/api/user/newUser', {
-                username,
-                email,
-                password
+                username: username,
+                email: email,
+                passwordHash: password
             });
-            console.log(response.data);
+            console.log(response.data); // faça algo com a resposta, como redirecionar o usuário para a página de login
         } catch (error) {
             console.error('Erro ao cadastrar:', error);
         }
     };
-
+ 
     return (
         <div className="corpo-cadastro">
             <div className="form-container">
@@ -37,5 +37,5 @@ const Cadastro = () => {
         </div>
     );
 }
-
+ 
 export default Cadastro;
