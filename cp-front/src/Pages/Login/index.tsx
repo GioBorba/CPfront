e56972e9317789479a35fdf5236426f00e340b55
Pedro Jesus <Pedromra.04@gmail.com>
2024-04-19 10:28:40 -0300

    fix login

--- cp-front/src/Pages/Login/index.tsx
+++ cp-front/src/Pages/Login/index.tsx
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import './style.css';
+import "./style.css";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "https://caiohalbert.bsite.net/api/user/login";
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post('https://caiohalbert.bsite.net/api/user/login', {
-                username,
-                password
-            });
-            console.log(response.data);
-            alert('Login bem-sucedido!'); 
-        } catch (error) {
-            console.error('Erro ao fazer login:', error);
-            alert('Erro ao fazer login. Verifique suas credenciais e tente novamente.'); 
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      console.log(email, password);
 
-    return (
-        <div className="corpo-login">
-            <div className="form-container">
-                <h1 className='log'>Login</h1>
-                <input type="text" placeholder="Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button className="button login" onClick={handleLogin}>
-                    Logar
-                </button>
-            </div>
-            <Link to="/" className="link-home">Voltar para a página inicial</Link>
-        </div>
-    );
-}
+      const response = await axios.post(url, {
+        email: email,
+        password: password,
+      });
+      console.log(response.data);
+      alert("Login bem-sucedido!");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      alert(
+        "Erro ao fazer login. Verifique suas credenciais e tente novamente."
+      );
+    }
+  };
+
+  return (
+    <div className="corpo-login">
+      <div className="form-container">
+        <h1 className="log">Login</h1>
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button login" onClick={handleLogin}>
+          Logar
+        </button>
+      </div>
+      <Link to="/" className="link-home">
+        Voltar para a página inicial
+      </Link>
+    </div>
+  );
+};
 
 export default Login;
